ff50e219eedeb56fdfc49bb0e7920b795dc79899
langletamelie <amelie.langlet@hotmail.com>
2018-07-12 12:11:41 +0200

    correction de la regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var form = angular.module('validForm', []);
 form.controller('validCtrl',['$scope',  function($scope){
-  $scope.regex = '(.)?@(.)?';
+  $scope.regex = '((.)+)?@((.)+)?';
   $scope.resultTestMail = false;
   $scope.testMail = function(){
     $scope.resultTestMail = $scope.form.email.$valid;
